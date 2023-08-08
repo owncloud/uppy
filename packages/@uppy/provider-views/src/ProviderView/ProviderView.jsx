25dadb12e069c557b3b9d900a54ebed4db5659ef
Dominik Schmidt <dev@dominik-schmidt.de>
2023-08-08 14:32:19 +0200

    @uppy/provider-view: allow apps to render custom forms in AuthView

--- packages/@uppy/provider-views/src/ProviderView/ProviderView.jsx
+++ packages/@uppy/provider-views/src/ProviderView/ProviderView.jsx
@@ -242,7 +242,18 @@ export default class ProviderView extends View {
     this.plugin.setPluginState({ filterInput: '' })
   }
 
-  async handleAuth () {
+  async handleAuth (event) {
+    event.preventDefault()
+
+    const formData = new FormData(event.target)
+    this.provider.cutomQueryParams = Object.fromEntries(formData.entries())
+    this.opts.authInputs?.forEach(i => {
+      if (!i.serialize) {
+        return
+      }
+      this.provider.cutomQueryParams[i.name] = i.serialize(this.provider.cutomQueryParams[i.name])
+    })
+
     const clientVersion = `@uppy/provider-views=${ProviderView.VERSION}`
     try {
       await this.provider.login({ uppyVersions: clientVersion })
@@ -478,6 +489,7 @@ export default class ProviderView extends View {
             handleAuth={this.handleAuth}
             i18n={this.plugin.uppy.i18n}
             i18nArray={this.plugin.uppy.i18nArray}
+            inputs={this.opts.authInputs}
           />
         </CloseWrapper>
       )
